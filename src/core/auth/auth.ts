40545a449753f7ff4d73192dbbb26b5d2602840c
tiagodlb <tigolimab223@gmail.com>
2025-11-09 11:26:11 -0300

    feat: auth

--- src/core/auth/auth.ts
+++ src/core/auth/auth.ts
@@ -1,17 +1,44 @@
-import { betterAuth } from 'better-auth'
-import {  PostgresDialect } from 'kysely'
-import { Pool } from 'pg'
+import { betterAuth } from "better-auth";
+import { PostgresDialect } from "kysely";
+import { openAPI } from 'better-auth/plugins'
+import { Pool } from "pg";
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    database: 'bemusicseeker',
-    host: 'localhost',
-    user: 'postgres', 
+    database: "bemusicseeker",
+    host: "localhost",
+    user: "postgres",
     port: 51213,
     max: 10,
-  })
-})
+  }),
+});
 
 export const auth = betterAuth({
-  database: dialect
-})
+  plugins: [openAPI()],
+  basePath: 'api',
+  database: dialect,
+});
+
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>
+const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema())
+
+export const OpenAPI = {
+    getPaths: (prefix = '/auth/api') =>
+        getSchema().then(({ paths }) => {
+            const reference: typeof paths = Object.create(null)
+
+            for (const path of Object.keys(paths)) {
+                const key = prefix + path
+                reference[key] = paths[path]!
+
+                for (const method of Object.keys(paths[path]!)) {
+                    const operation = (reference[key] as any)[method]
+
+                    operation.tags = ['Better Auth']
+                }
+            }
+
+            return reference
+        }) as Promise<any>,
+    components: getSchema().then(({ components }) => components) as Promise<any>
+} as const
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,34 @@
 import { Elysia } from "elysia";
-import { auth_guard } from "./core/security/auth_guards";
+import { OpenAPI } from "./core/auth/auth";
+import { openapi } from "@elysiajs/openapi";
+import { cors } from "@elysiajs/cors";
+import { betterAuth } from "./core/auth/macro";
 
 const server = new Elysia()
-  .group("/v1", (app) => app.use(auth_guard).get("/", "hi"))
+  .use(
+    openapi({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    })
+  )
+  .use(
+    cors({
+      origin: "http://localhost:3001",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  )
+  .group("/v1", (app) =>
+    app
+      .use(betterAuth)
+      .get("/", "hi\n")
+      .get("/user", ({ user }) => user, {
+        auth: true,
+      })
+  )
   .listen(3000);
 
 console.log("Running on http://localhost:3000");
